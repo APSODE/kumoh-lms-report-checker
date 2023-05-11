98a3b3ba62ce34a749366c7ac33cfa8724589c35
APSODE <2gunbo74@gmail.com>
2023-05-11 17:15:05 +0900

    Database에 row추가, update, commit하는 기능 구현

--- src/Database/DatabaseController.py
+++ src/Database/DatabaseController.py
@@ -1,12 +1,30 @@
-from typing import Type
+from typing import Type, Any, Union
 from src.Database.DatabaseCreator import DatabaseCreator
+from src.Database.Model.ReportModel import ReportModel
+from src.Database.Model.SubjectModel import SubjectModel
+from sqlalchemy import ClauseElement
 
 
 class DatabaseController:
-    def __init__(self, model_object: Type[DatabaseCreator]):
-        self._model_object = model_object
+    def __init__(self, db_creator: DatabaseCreator):
+        self._database_creator = db_creator
 
     @property
-    def ModelObject(self) -> Type[DatabaseCreator]:
-        return self._model_object
+    def DB_Creator(self):
+        return self._database_creator
+
+    def AddData(self, model_object: Type[DatabaseCreator.Model]):
+        self._database_creator.Session.add(model_object)
+
+    def CommitData(self):
+        self._database_creator.Session.commit()
+
+    def UpdateData(self, model_class: Type[DatabaseCreator.Model], filter_data: bool, update_data: dict):
+        self._database_creator.Session.query(
+            model_class
+        ).filter(
+            filter_data
+        ).update(
+            update_data
+        )
 
