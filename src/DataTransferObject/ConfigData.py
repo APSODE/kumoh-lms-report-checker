355eacc24ae5bb1f74388f507f865db294005a3f
APSODE <2gunbo74@gmail.com>
2023-05-15 11:15:52 +0900

    Config의 데이터를 Object로 다룰수 있도록 하는 ConfigData DTO

---
+++ src/DataTransferObject/ConfigData.py
@@ -0,0 +1,52 @@
+from typing import Optional, Dict, Union, Any
+from src.Bot.utils.JsonReadWrite import JsonReadWrite
+from src.DataTransferObject.BaseDataTransferObject import BaseDataTransferObject
+from src.CustomException.DTOException import *
+
+
+class ConfigData(BaseDataTransferObject):
+    def __init__(self, file_dir: Optional[str] = None, config_data_dict: Optional[Dict[str, Any]] = None):
+        self._config_file_data = JsonReadWrite.ReadJson(file_dir) if config_data_dict is None else config_data_dict
+
+        time_data_temp = self._config_file_data.get("time_data")
+        update_check_term_temp = self._config_file_data.get("update_check_term")
+
+        self._notice_time_data = time_data_temp if time_data_temp is not None else {"hour": 0, "minute": 0}
+        self._update_check_term = update_check_term_temp if update_check_term_temp is not None else 10
+
+    @property
+    def NoticeTime(self) -> Dict[str, int]:
+        return self._notice_time_data
+
+    @NoticeTime.setter
+    def NoticeTime(self, new_time_data: Dict[str, int]):
+        if new_time_data.get("hour") is None or new_time_data.get("minute") is None:
+            raise ConfigDataSetterNotExistError()
+
+        self._notice_time_data = new_time_data
+
+    @property
+    def UpdateCheckTerm(self) -> int:
+        return self._update_check_term
+
+    @UpdateCheckTerm.setter
+    def UpdateCheckTerm(self, new_check_term: int):
+        if not new_check_term >= 0:
+            raise ConfigDataInvalidSetterDataError()
+
+        self._update_check_term = new_check_term
+
+    def GetAllDataByDict(self) -> dict:
+        return {key.replace("_", "", 1): value for key, value in self.__dict__.items()}
+
+    @staticmethod
+    def CreateObject(config_file_dir: str) -> "ConfigData":
+        return ConfigData(
+            file_dir = config_file_dir
+        )
+
+    @staticmethod
+    def SerializeData(data_dict: Dict[str, Any]) -> "ConfigData":
+        return ConfigData(
+            config_data_dict = data_dict
+        )
