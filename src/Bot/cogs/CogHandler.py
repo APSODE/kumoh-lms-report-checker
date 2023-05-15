86e3f2061d41fdc0af4330155dc9e2b5e02ac382
APSODE <2gunbo74@gmail.com>
2023-05-15 11:17:41 +0900

    Bot에서 사용하는 cog를 등록 및 관리하는 Handler

---
+++ src/Bot/cogs/CogHandler.py
@@ -0,0 +1,17 @@
+import os
+from discord.ext import commands
+
+
+class CogHandler:
+    def __init__(self, bot_object: commands.Bot):
+        self._bot = bot_object
+
+    async def setup_cogs(self):
+        for cog_file in os.listdir(".\\src\\Bot\\cogs\\"):
+            if cog_file in ["CogHandler.py", "BaseCommand.py"]:
+                pass
+            else:
+                try:
+                    await self._bot.load_extension(f"src.Bot.cogs.{cog_file[:-3]}")
+                except Exception as MSG:
+                    print(f"{cog_file}을 cog 등록에 실패하였습니다.")
