5eedcb1d61a7869e06ba079fa40c7c97bca18f22
APSODE <2gunbo74@gmail.com>
2023-05-18 09:38:21 +0900

    ConfigData DTO 구성 수정으로 인한 메소드 구현 수정

--- src/Bot/func/NoticeFunc.py
+++ src/Bot/func/NoticeFunc.py
@@ -1,39 +1,27 @@
+import os
 from typing import Optional, Dict, List
 from src.DataTransferObject.ConfigData import ConfigData
+from src.Bot.func.ConfigFunc import ConfigFunc
 from src.Bot.utils.JsonReadWrite import JsonReadWrite
 from discord import Embed
 
 
-CONFIG_FILE_DIR = ".\\src\\Bot\\config.json"
-
-
 class NoticeFunc:
     @staticmethod
-    def GetConfigDataObject() -> ConfigData:
-        return ConfigData.CreateObject(config_file_dir = CONFIG_FILE_DIR)
-
-    @staticmethod
-    def SetConfigData(data_object: ConfigData):
-        JsonReadWrite.WriteJson(
-            write_data = data_object.GetAllDataByDict(),
-            write_target_dir = CONFIG_FILE_DIR
-        )
-
-    @staticmethod
     def SetNoticeTimeData(time_datas: List[Dict[str, int]]):
-        current_config_data = NoticeFunc.GetConfigDataObject()
+        current_config_data = ConfigFunc.GetConfigDataObject()
         current_config_data.NoticeTime = time_datas
 
-        NoticeFunc.SetConfigData(
+        ConfigFunc.SetConfigData(
             data_object = current_config_data
         )
 
     @staticmethod
     def SetUpdateCheckTerm(update_check_term: int):
-        current_config_data = NoticeFunc.GetConfigDataObject()
+        current_config_data = ConfigFunc.GetConfigDataObject()
         current_config_data.UpdateCheckTerm = update_check_term
 
-        NoticeFunc.SetConfigData(
+        ConfigFunc.SetConfigData(
             data_object = current_config_data
         )
 
