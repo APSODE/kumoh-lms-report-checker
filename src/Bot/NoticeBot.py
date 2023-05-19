f81180892226578b6070ca82e84ddf6fd5e3637f
APSODE <2gunbo74@gmail.com>
2023-05-19 16:06:05 +0900

    봇 구현 방식 변경으로 인한 봇 메인 파일 수정

--- src/Bot/NoticeBot.py
+++ src/Bot/NoticeBot.py
@@ -1,10 +1,8 @@
-import os.path
-
 import discord
 from discord.ext import commands, tasks
 from discord import Intents
-from src.Bot.utils.TokenIdDataCreator import TokenIdDataCreator
-from src.Bot.cogs.CogHandler import CogHandler
+from src.Bot.core.ConfigManager import ConfigManager
+from src.Bot.core.CogManager import CogManager
 
 
 class NoticeBot(commands.Bot):
@@ -17,27 +15,24 @@ class NoticeBot(commands.Bot):
             intents = default_intents,
             sync_command = True
         )
-        self._cog_handler = CogHandler(
+
+        self._cog_manager = CogManager(
             bot_object = self
         )
-        self._token_id_data_object = TokenIdDataCreator.CreateTokenData()
-        self._start()
+
+        self._config_manager = ConfigManager()
 
     def _start(self):
         self.run(
-            self._token_id_data_object.Token
+            self._config_manager.ConfigDTO.Token
         )
 
     async def on_ready(self):
         print("봇이 정상적으로 실행되었습니다.")
         current_bot_activity = discord.Game("과제")
-        await self.change_presence(status = discord.Status.online, activity = current_bot_activity)
-        await self._cog_handler.setup_cogs()
 
-
-
-
-
-
-
-
+        await self.change_presence(
+            status = discord.Status.online,
+            activity = current_bot_activity
+        )
+        await self._cog_manager.setup_cogs()
