aaaf73b3ca4e9f4e26a50193202db77cf168769b
APSODE <2gunbo74@gmail.com>
2023-05-25 10:39:57 +0900

    ConfigManager객체의 사용을 위해 생성자 파라미터 추가 및 cog등록 코드 수정

--- src/Bot/cogs/NoticeCommand.py
+++ src/Bot/cogs/NoticeCommand.py
@@ -3,10 +3,17 @@ import asyncio
 from discord.ext.commands import Context
 from discord.ext import commands
 from discord import Embed
+from src.Bot.core.ConfigManager import ConfigManager
 from src.Bot.cogs.BaseCommand import BaseCommand
+from src.Func.NoticeCommandHandler import NoticeCommandHandler
+from src.Func.NoticeFunc import NoticeFunc
+from src.Bot.NoticeBot import NoticeBot
 
 
 class NoticeCommand(BaseCommand):
+    def __init__(self, config_manager: ConfigManager):
+        self._config_manager = config_manager
+
     @commands.command(aliases = ["알림", "ㅇㄹ", "df", "dkffla"])
     async def Notice(self, ctx: Context, *args):
         ica_task = asyncio.create_task(self.CommandArgumentDevider(args))
@@ -25,3 +32,7 @@ class NoticeCommand(BaseCommand):
     @property
     def FileName(self):
         return os.path.basename(__file__)
+
+
+async def setup(bot: NoticeBot):
+    await bot.add_cog(NoticeCommand(config_manager = bot.ConfigManager))
