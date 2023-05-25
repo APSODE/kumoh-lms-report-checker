897a91625b78f60414ec67206ee6c79d7e45e4c7
APSODE <2gunbo74@gmail.com>
2023-05-25 10:42:41 +0900

    알림 명령어의 실제 기능 구현 클래스

--- src/Func/NoticeFunc.py
+++ src/Func/NoticeFunc.py
@@ -1,5 +1,8 @@
 from typing import Optional
 from datetime import datetime
+
+import discord
+
 from src.DataTransferObject.ConfigData import ConfigData
 
 
@@ -20,4 +23,32 @@ class NoticeFunc:
             else:
                 return False
 
-    
+    @staticmethod
+    def NoticeCommandsInfo() -> discord.Embed:
+        rt_embed = discord.Embed(
+            title = "**`!알림`** 명령어 목록"
+            # description = "**`!알림`** 명령어의 정보입니다."
+        )
+
+        rt_embed.add_field(
+            name = "1. 시간",
+            value = "알림 예약 시간데이터를 확인 및 설정합니다.\n사용법 : `!알림 시간 <설정 / 확인>`"
+        )
+
+        rt_embed.add_field(
+            name = "2. 갱신주기",
+            value = "과제 데이터 갱신 주기를 확인 및 설정합니다.\n사용법 : `!알림 갱신주기 <설정 / 확인>`"
+        )
+
+        return rt_embed
+
+    @staticmethod
+    def ShowNoticeTimeData() -> discord.Embed:
+        rt_embed = discord.Embed(
+            title = "현재 설정된 알림 시간 목록 입니다."
+
+        )
+
+
+
+
